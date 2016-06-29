835e79e5fa374a87db1e3c0d409e5674a91a9c11
Hussain <hussain_bri2@yahoo.com>
2016-06-29 17:23:33 +0530

    Connected DB

--- app/src/main/java/com/example/hussain/bloodconnectserver/add_camp.java
+++ app/src/main/java/com/example/hussain/bloodconnectserver/add_camp.java
@@ -1,33 +1,118 @@
 package com.example.hussain.bloodconnectserver;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.data;
 
 public class add_camp extends AppCompatActivity {
     Button b;
     String name;
     EditText ed1;
-    select_camp obj=new select_camp();
+    private String URL_NEW_PREDICTION = "http://blloodconnect.net16.net/camp_phpcode.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_camp);
         b=(Button)findViewById(R.id.submit);
         ed1=(EditText)findViewById(R.id.editText2);
-        name=ed1.getText().toString();
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                name=ed1.getText().toString();
+                if(ed1.length()!=0)
+                new AddNewPrediction().execute(name);
                 Toast.makeText(getApplicationContext(),"Camp Created!",Toast.LENGTH_SHORT).show();
                Intent i=new Intent(add_camp.this,select_camp.class);
-                startActivity(i);
+               // startActivity(i);
+                i.putExtra("args", name);
+                setResult(Activity.RESULT_OK, i);
                 finish();
+
             }
     });
+                //Write your code if there's no result
+            }
+    private class AddNewPrediction extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(String... arg) {
+            // TODO Auto-generated method stub
+            String name = arg[0];
+
+
+
+            // Preparing post params
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("name", name));
+
+            Sender serviceClient = new Sender();
+
+            String json = serviceClient.makeServiceCall(URL_NEW_PREDICTION,
+                    Sender.POST, params);
+
+            Log.d("Create Prediction Request: ", "> " + json);
+
+            if (json != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(json);
+                    boolean error = jsonObj.getBoolean("error");
+                    // checking for error node in json
+                    if (!error) {
+                        // new category created successfully
+                        Log.e("Prediction added successfully ",
+                                "> " + jsonObj.getString("message"));
+                    } else {
+                        Log.e("Add Prediction Error: ",
+                                "> " + jsonObj.getString("message"));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Log.e("JSON Data", "JSON data error!");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+    }
+
+
+
+
+
+
 }
-}
+
--- app/src/main/java/com/example/hussain/bloodconnectserver/select_camp.java
+++ app/src/main/java/com/example/hussain/bloodconnectserver/select_camp.java
@@ -1,24 +1,56 @@
 package com.example.hussain.bloodconnectserver;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.attr.id;
+
 public class select_camp extends AppCompatActivity {
     ListView listView;
-String pass;
-    boolean  checked=false;
-public  List<String> list;
+    String pass;
+    boolean checked = false;
+    public List<String> list;
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+
+    private static final String TAG_NAME = "name";
+
+
+
+
+    JSONArray peoples = null;
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,42 +62,142 @@ public  List<String> list;
         listView = (ListView) findViewById(R.id.listview_with_fab);
         list = new ArrayList<String>();
 
-        list.add(" ");
-        list.add("Camp 1");
+
+       /* list.add("Camp 1");
         list.add("Camp 2");
         list.add("Camp 3");
         list.add("Camp 4");
         list.add("Camp 5");
         list.add("Camp 6");
         list.add("Camp 7");
-        list.add("Camp 8");
+        list.add("Camp 8");*/
+       // list.add(" ");
+     //   list.add(" ");
+        getData();
 
 
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, android.R.id.text1, list);
-                listView.setAdapter(adapter);
+     adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, list);
+        listView.setAdapter(adapter);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(select_camp.this,add_camp.class);
-                startActivity(intent);
-                finish();
-
-
+                Intent intent = new Intent(select_camp.this, add_camp.class);
+                startActivityForResult(intent, 4);
+                //finish();
 
 
             }
         });
     }
 
+  /*  @Override
+   /* protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==4)
+            Log.i("info","Working");
+    }*/
+@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+       // Toast.makeText(getApplicationContext(),"Called",Toast.LENGTH_SHORT).show();
+        if (requestCode == 4) {
+            if (resultCode == Activity.RESULT_OK) {
+                String result = data.getStringExtra("args");
+
+                list.add(result);
+                adapter.notifyDataSetChanged();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+                Toast.makeText(getApplicationContext(),"why u do dis?",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i=0;i<peoples.length();i++){
+                JSONObject c = peoples.getJSONObject(i);
+
+                String name = c.getString(TAG_NAME);
+
+                list.add(name);
+                adapter.notifyDataSetChanged();
 
 
+
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+                HttpPost httppost = new HttpPost("http://blloodconnect.net16.net/t1.php");
+
+                // Depends on your web service
+                httppost.setHeader("Content-type", "application/json");
+
+                InputStream inputStream = null;
+                String result = null;
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity entity = response.getEntity();
+
+                    inputStream = entity.getContent();
+                    // json is UTF-8 by default
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (Exception e) {
+                    // Oops
+                }
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute();
+    }
 
 
 
 
 }
+
+
+
+
+
+
+
+
